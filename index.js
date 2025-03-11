666800e23f82ae323567f1a47bef31fe491b68d3
noor <noorhafowbare@gmail.com>
2025-03-11 12:18:49 +0300

    initial commit

---
+++ index.js
@@ -0,0 +1,32 @@
+// comparisson operators
+
+// comparisson operators are divided in to two categories
+
+// 1. relational operators
+// 2. equality operators
+
+//  relational operators
+// these are relational operator
+
+// 1. > greater than
+// 2. < less than
+// 3. >= greater than or equal to
+// 4. <= less than or equal to
+
+console.log(10 > 10);
+console.log(10 < 10);
+console.log(10 >= 10);
+console.log(10 <= 10);
+
+// equality operators are:
+// strict equality which will check (type + a value)
+//strict non-equality operators (type + value)
+//lose equality operator (value)
+// lose not equality operator (value)
+
+console.log(10 === 10);
+console.log(10 === "10");
+console.log(10 !== 10);
+console.log(10 !== "10");
+console.log(10 == 10);
+console.log(10 !== 10);
